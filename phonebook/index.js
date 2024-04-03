6c8fc1919b8e27c2c0a7089c063636b9329d641c
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-04-03 19:49:41 +0400

    Solution for 3.3: Phonebook backend step 3

--- phonebook/index.js
+++ phonebook/index.js
@@ -39,6 +39,22 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+  const person = persons.find((person) => {
+    console.log(person.id, typeof person.id, id, typeof id);
+    return person.id === id;
+  });
+  console.log(person);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
