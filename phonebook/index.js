be0a39e7cffa568d7ae703baec815646ec67015f
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-04-05 20:23:44 +0400

    Copy production build of the frontend to the backend

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json()); //express.json() is middleware. This middleware is responsible for parsing incoming request bodies with JSON payloads.
 
 app.use(morgan("tiny"));
+
+app.use(cors());
+
+app.use(express.static("dist"));
 
 let persons = [
   {
@@ -104,6 +109,6 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
